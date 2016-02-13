dcd79a2ae1197f971eaa258a5d69c1ba49c19435
Anthony Desmier <adesmier@gmail.com>
2016-02-13 14:35:19 +0000

    Continued with jQuery navigation and animations

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,3 +1,67 @@
-$(document).ready(function(){
-    $('js-features-section').waypoints(function(direction))
-})
+$(document).ready(function () {
+    
+    /***enable sticky navigation menu***/
+    $('.js-features-section').waypoint(function (direction) {
+        if (direction == 'down') {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px'
+    });
+    
+    /***scroll to sign-up section***/
+    $('.js-scroll-to-plans').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js-sign-up').offset().top - 60
+        }, 1000);
+    });
+    
+    /***scroll to features section***/
+    $('.js-scroll-to-features').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js-features-section').offset().top - 60
+        }, 1000);
+    });
+    
+    /***naviagation scroll***/
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top - 60
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+    
+    /***features animation***/
+    $('.js-features-section').waypoint(function (direction) {
+        $('.js-animate-features').addClass('animated fadeIn');
+    }, {
+        offset: '200px'
+    });
+    
+    /***iphone animation***/
+    $('.js-works-section').waypoint(function (direction) {
+        $('.js-animate-works-phone').addClass('animated fadeInLeft');
+    }, {
+        offset: '400px'
+    });
+    
+    /***works steps animation***/
+    $('.js-works-section').waypoint(function (direction) {
+        $('.js-animate-works-steps').addClass('animated fadeInRight');
+    }, {
+        offset: '400px'
+    });
+    
+    
+    
+});
